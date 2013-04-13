5518965a819b464a0b0b2fcc2f59673d16950862
Keisuke Kagoshima <ca54makske@gmail.com>
2013-04-13 13:14:41 +0900

    modify path

---
+++ ios/OthermanRecords/OthermanRecords/FavoriteListViewController.h
@@ -0,0 +1,13 @@
+//
+//  FavoriteListViewController.h
+//  OthermanRecords
+//
+//  Created by ca54makske on 13/04/13.
+//  Copyright (c) 2013年 Otherman-Records. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FavoriteListViewController : UITableViewController
+
+@end
